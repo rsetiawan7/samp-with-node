4235ade43776f0f38d7950d19c63523f941d9a52
Fuat Akbar <fuat@deeeplabs.id>
2022-03-01 12:19:53 +0700

    Test: trying to use node_modules package into index.js (but failed)

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+import randomString from "random-string";
+console.log(randomString());
+
 samp.on(EVENT_PLAYER_CONNECT, (playerid) => {
-    let name = samp.callNative("GetPlayerName", "iSi", playerid, 24);
-    samp.callNative("SendClientMessageToAll", "is", -1, "{006600}[JOIN] {ffffff}" + name + " bergabung ke dalam server.");
+    samp.callNative('SendClientMessage', 'iis', playerid, 0xff00ffff, "Welcome to the server player " + playerid);
 });
